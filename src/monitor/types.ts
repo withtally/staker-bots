6c8aca0167b87a1de50d25b7361a2a6913873be2
Sero <69639595+Seroxdesign@users.noreply.github.com>
2025-02-10 10:18:48 -0500

    envs

--- src/monitor/types.ts
+++ src/monitor/types.ts
@@ -3,10 +3,10 @@ import { IDatabase, ProcessingCheckpoint } from '@/database';
 
 export interface MonitorConfig {
   provider: ethers.Provider;
-  stakerAddress: string,
-  arbTokenAddress: string,
-  rewardCalculatorAddress: string,
-  rewardNotifierAddress: string,
+  stakerAddress: string;
+  arbTokenAddress: string;
+  rewardCalculatorAddress: string;
+  rewardNotifierAddress: string;
   networkName: string;
   chainId: number;
   startBlock: number;
