110df906c7b76703d6b3175ea3fc2a1b86b577fe
Sero <69639595+Seroxdesign@users.noreply.github.com>
2025-02-12 07:13:47 -0500

    calculator schema

--- src/database/supabase/migrations/schema.sql
+++ src/database/supabase/migrations/schema.sql
@@ -14,7 +14,6 @@ CREATE TABLE IF NOT EXISTS processing_checkpoints (
     last_block_number BIGINT NOT NULL,
     block_hash TEXT NOT NULL,
     last_update TIMESTAMP WITH TIME ZONE DEFAULT TIMEZONE('utc', NOW()) NOT NULL,
-    last_block BIGINT
 );
 
 -- Create indexes for better query performance
