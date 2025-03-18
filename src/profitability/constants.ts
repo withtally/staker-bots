b80adc1ebf3174935e2e2ed4f3b4d1482809e6be
seroxdesign <sherifcherfa@gmail.com>
2025-03-18 13:07:01 -0400

    wrap up

--- src/profitability/constants.ts
+++ src/profitability/constants.ts
@@ -1,5 +1,5 @@
 export const STAKER_ABI = [
-  'function bumpEarningPower(uint256 depositId, uint256 tip) returns (uint256)',
+  'function bumpEarningPower(uint256 depositId, address tipReceiver, uint256 tip) returns (uint256)',
   'function unclaimedReward(uint256 depositId) view returns (uint256)',
   'function maxBumpTip() view returns (uint256)',
 ];
